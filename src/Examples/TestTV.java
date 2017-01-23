e0a96eea7b4966571cf2af45b8519affea66e62c
benjamhooper <bhooper11231@gmail.com>
2017-01-22 23:24:42 -0600

    Added function

--- src/Examples/TestTV.java
+++ src/Examples/TestTV.java
@@ -4,6 +4,22 @@ package Examples;
  * @author Benjamin Hooper
  */
 public class TestTV {
-public sta
+public static void main(String[] args) {
+    TV tv1 = new TV();
+    tv1.turnOn();
+    tv1.setChannel(30);
+    tv1.setVolume(3);
+    
+    TV tv2 = new TV();
+    tv2.turnOn();
+    tv2.channelUp();
+    tv2.channelUp();
+    tv2.volumeUp();
+    
+    System.out.println("TV1's channel is " + tv1.channel
+            + " and volume level is " + tv1.volumeLevel);
+    System.out.println("TV2's channel is " + tv2.channel
+            + "and volume level is " + tv2.volumeLevel);
+ }
 }
 
