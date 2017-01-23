216cbb7929e47de13d5d3c0c7b17bb91aaf7c2cc
benjamhooper <bhooper11231@gmail.com>
2017-01-23 17:09:45 -0600

    Added outputs

--- src/Lab01/TestRectangle.java
+++ src/Lab01/TestRectangle.java
@@ -9,4 +9,25 @@ package Lab01;
 
 public class TestRectangle {
 
+public static void main(String[] args){
+    Rectangle rectangle1 = new Rectangle(4, 40);
+    Rectangle rectangle2 = new Rectangle(3.5, 35.9);
+    
+    System.out.println("---------------");
+    System.out.println("- Rectangle 1 -");
+    System.out.println("---------------");
+    System.out.println("Width: " + rectangle1.width);
+    System.out.println("Length: " + rectangle1.length);
+    System.out.printf("Area: %3.3f\n", rectangle1.getArea());
+    System.out.printf("Perimeter: %3.3f\n", rectangle1.getPerimeter());
+    
+    System.out.println("---------------");
+    System.out.println("- Rectangle 2 -");
+    System.out.println("---------------");
+    System.out.println("Width: " + rectangle2.width);
+    System.out.println("Length: " + rectangle2.length);
+    System.out.printf("Area: %3.3f\n", rectangle2.getArea());
+    System.out.printf("Perimeter: %3.3f\n", rectangle2.getPerimeter());
+    
+ }
 }
