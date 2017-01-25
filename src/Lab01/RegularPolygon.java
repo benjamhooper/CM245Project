12dcd1c725deca78892db30eba9a579cd9eeb46b
benjamhooper <bhooper11231@gmail.com>
2017-01-25 12:45:59 -0600

    Part two of Lab01

---
+++ src/Lab01/RegularPolygon.java
@@ -0,0 +1,21 @@
+/*
+* Rectangle 
+* Takes width and height calculates the area    
+* @author Benjamin Hooper
+* @version 1.0
+ */
+package Lab01;
+
+public class RegularPolygon {
+
+    private int n;
+    private double side;
+    private double x;
+    private double y;
+
+    RegularPolygon() {
+        n = 3;
+        side = 1;
+        x = y = 0;
+    }
+}
---
+++ src/Lab01/TestPolygon.java
@@ -0,0 +1,18 @@
+package Lab01;
+
+/**
+ * @author Benjamin Hooper
+ */
+public class TestPolygon {
+
+    public static void main(String[] args) {
+        System.out.println("Lab01 Benjamin Hooper");
+        System.out.println("Printing Polygon -------------");
+        System.out.printf("Polygon: perimeter: %3.3f");
+        System.out.println("Polygon: Number of Sides: ");
+        System.out.printf("Polygon: Side Length: %3.3f");
+        System.out.printf("Polygon: x-coordinate: %3.3f");
+        System.out.printf("Polygon: y-coordinate: %3.3f");
+
+    }
+}
