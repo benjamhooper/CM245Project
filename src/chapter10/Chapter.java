836b10dfbe19092f7fbcf2f5a6890a292c14d1e0
benjamhooper <bhooper11231@gmail.com>
2017-02-01 12:08:10 -0600

    Updated from class

--- src/chapter10/Chapter.java
+++ src/chapter10/Chapter.java
@@ -1,17 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package chapter10;
 
-/**
- *
- * @author hooperbe
- */
 public class Chapter {
     private String name;
     private int pageCt;
-    
-            
+    public Chapter() { }
+    public Chapter(String name, int pageCt) {
+        setName(name);
+        setPageCt(pageCt);
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the pageCt
+     */
+    public int getPageCt() {
+        return pageCt;
+    }
+
+    /**
+     * @param pageCt the pageCt to set
+     */
+    public void setPageCt(int pageCt) {
+        this.pageCt = pageCt;
+    }
 }
