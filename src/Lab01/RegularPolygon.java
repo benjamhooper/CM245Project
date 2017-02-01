ebffbb62d6ffa7d7c93e752f71d9c82647421c14
benjamhooper <bhooper11231@gmail.com>
2017-02-01 12:47:51 -0600

    Final Changes before lab submit

--- src/Lab01/RegularPolygon.java
+++ src/Lab01/RegularPolygon.java
@@ -18,4 +18,67 @@ public class RegularPolygon {
         side = 1;
         x = y = 0;
     }
+	RegularPolygon(int newN, double newSide) {
+		n = newN;
+		side = newSide;
+		x = y = 0;
+	}
+
+	RegularPolygon(int newN, double newSide, double newX, double newY) {
+		n = newN;
+		side = newSide;
+		x = newX;
+		y = newY;
+	}
+
+	/** Methods */
+	/** Sets new n */
+	public void setN(int newN) {
+		n = newN;
+	}
+
+	/** Sets new length of the side */
+	public void setSide(double newSide) {
+		side = newSide;
+	}
+
+	/** Sets new x-coordinate */
+	public void setX(double newX) {
+		x = newX;
+	}
+
+	/** Sets new y-coordinate */
+	public void setY(double newY) {
+		y = newY;
+	}
+
+	/** Returns n */
+	public int getN() {
+		return n;
+	}
+
+	/** Returns the length of the side */
+	public double getSide() {
+		return side;
+	}
+
+	/** Returns x-coordinate */
+	public double getX() {
+		return x;
+	}
+
+	/** Returns y-coordinate */
+	public double getY() {
+		return y;
+	}
+
+	/** Returns the perimeter of the polygon */
+	public double getPerimeter() {
+		return side * n;
+	}
+
+	/** Returns the area of the polygon */
+	public double getArea() {
+		return (n * Math.pow(side, 2)) / (4 * Math.tan(Math.PI / n));
+	}
 }
--- src/Lab01/TestPolygon.java
+++ src/Lab01/TestPolygon.java
@@ -6,13 +6,31 @@ package Lab01;
 public class TestPolygon {
 
     public static void main(String[] args) {
+       RegularPolygon polygon1 = new RegularPolygon(6, 4);
+       RegularPolygon polygon2 = new RegularPolygon(10, 4);
+       RegularPolygon polygon3 = new RegularPolygon(3, 2);
         System.out.println("Lab01 Benjamin Hooper");
-        System.out.println("Printing Polygon -------------");
-        System.out.printf("Polygon: perimeter: %3.3f");
-        System.out.println("Polygon: Number of Sides: ");
-        System.out.printf("Polygon: Side Length: %3.3f");
-        System.out.printf("Polygon: x-coordinate: %3.3f");
-        System.out.printf("Polygon: y-coordinate: %3.3f");
-
+        System.out.println("Printing Polygon 1 -------------");
+        System.out.printf("Polygon: Area: %3.3f\n", polygon1.getArea());
+        System.out.printf("Polygon: Perimeter: %3.3f\n", polygon1.getPerimeter());
+        System.out.println("Polygon: Number of Sides: " + polygon1.getN());
+        System.out.printf("Polygon: Side Length: %3.3f\n", polygon1.getSide());
+        System.out.printf("Polygon: x-coordinate: %3.3f\n", polygon1.getX());
+        System.out.printf("Polygon: y-coordinate: %3.3f\n", polygon1.getY());
+        System.out.println("Printing Polygon 2 -------------");
+        System.out.printf("Polygon: Area: %3.3f\n", polygon2.getArea());
+        System.out.printf("Polygon: Perimeter: %3.3f\n", polygon2.getPerimeter());
+        System.out.println("Polygon: Number of Sides: " + polygon2.getN());
+        System.out.printf("Polygon: Side Length: %3.3f\n", polygon2.getSide());
+        System.out.printf("Polygon: x-coordinate: %3.3f\n", polygon2.getX());
+        System.out.printf("Polygon: y-coordinate: %3.3f\n", polygon2.getY());
+        System.out.println("Printing Polygon 3 -------------");
+        System.out.printf("Polygon: Area: %3.3f\n", polygon3.getArea());
+        System.out.printf("Polygon: Perimeter: %3.3f\n", polygon3.getPerimeter());
+        System.out.println("Polygon: Number of Sides: " + polygon3.getN());
+        System.out.printf("Polygon: Side Length: %3.3f\n", polygon3.getSide());
+        System.out.printf("Polygon: x-coordinate: %3.3f\n", polygon3.getX());
+        System.out.printf("Polygon: y-coordinate: %3.3f\n", polygon3.getY()); 
+        System.out.println("Exiting Program");
     }
 }
