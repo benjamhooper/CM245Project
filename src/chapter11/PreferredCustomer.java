c527af32229d1e90f946695593cb5a1afc90dbb6
benjamhooper <bhooper11231@gmail.com>
2017-02-09 09:39:45 -0600

    Added Inheritance Examples

---
+++ src/chapter11/PreferredCustomer.java
@@ -0,0 +1,31 @@
+
+package chapter11;
+
+public class PreferredCustomer extends Customer {
+    private double discount;
+    public PreferredCustomer() {
+        setDiscount(0.1);
+        
+    }
+    public PreferredCustomer(String name, String phone, 
+            double discount) {
+        super(name,phone);
+        setDiscount(discount);
+    }
+    
+    /**
+     * @return the discount
+     */
+    public double getDiscount() {
+        return discount;
+    }
+    
+
+    /**
+     * @param discount the discount to set
+     */
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+}
---
+++ src/chapter11/StandardCustomer.java
@@ -0,0 +1,6 @@
+
+package chapter11;
+
+public class StandardCustomer extends Customer {
+    public StandardCustomer() {}
+}
